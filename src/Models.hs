fb7f349660e5ca48f763a989293ad47041d939a0
Michael Neale <michael.neale@gmail.com>
2014-05-13 22:24:32 +1000

    RAGE COMMIT

--- src/Models.hs
+++ src/Models.hs
@@ -10,16 +10,53 @@ module Models where
 
 import GHC.Generics
 import Data.Aeson
+import Data.Text
 
-data Point = Point { _x :: Double, _y :: Double }
-   deriving (Show, Generic)
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
+import Data.Text.Encoding
 
-instance FromJSON Point
-instance ToJSON Point
+
+-- | Type of each JSON entry in record syntax.
+data Person =
+  Person { firstName  :: !Text
+         , lastName   :: !Text
+         , age        :: Int
+         , likesPizza :: Bool
+           } deriving (Show,Generic)
+
+-- Instances to convert our type to/from JSON.
+
+instance FromJSON Person
+instance ToJSON Person
+
+-- | Location of the local copy, in case you have it,
+--   of the JSON file.
+jsonFile :: FilePath
+jsonFile = "pizza.json"
+
+-- | URL that points to the remote JSON file, in case
+--   you have it.
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+-- Move the right brace (}) from one comment to another
+-- to switch from local to remote.
+
+{--
+-- Read the local copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+--}
+
+{--}
+-- Read the remote copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = simpleHttp jsonURL
+--}
 
 
-someJson = "{\"x\":3.0,\"y\":-1.0}"
-
-example1 :: Maybe Point
-example1 = decode someJson
-
+example1 = encodeUtf8 <$> getJSON
